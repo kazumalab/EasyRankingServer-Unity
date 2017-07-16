b11372669e6007087df275e507ad0e1b881fff5d
kazumalab <kazuma-arashi@hotmail.co.jp>
2017-07-16 21:46:44 +0900

    first commit

---
+++ Assets/RankingServer/Scripts/PostData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+
+namespace RankingServer {
+	public class PostData : MonoBehaviour {
+
+		[SerializeField]
+		public string _access_game_key;
+		[SerializeField]
+		public string _access_token;
+
+		private HttpWebRequest req;
+
+		// Use this for initialization
+		void Start () {
+			// req = new HttpWebRequest();
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			
+		}
+	}
+}
