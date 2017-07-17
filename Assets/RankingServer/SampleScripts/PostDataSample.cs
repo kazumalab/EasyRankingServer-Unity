6c71533300c404a183f3a333d6f287d65d693965
kazumalab <kazuma-arashi@hotmail.co.jp>
2017-07-17 19:45:07 +0900

    Add Sample Scene, create Class.

---
+++ Assets/RankingServer/SampleScripts/PostDataSample.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Specialized;
+using UnityEngine;
+using System.Net;
+using System.IO;
+using System;
+using UnityEngine.Events;
+using RankingServer;
+
+public class PostDataSample : MonoBehaviour {
+	private RankingSystem ranking_system;
+	[SerializeField]
+	private string access_game_key;
+	[SerializeField]
+	private string access_token;
+
+	void Start () {
+		ranking_system = new RankingSystem (access_game_key, access_token);
+		ranking_system.OnPost += () => print("完了");
+		ranking_system.Send ("kazumalab", "300");
+	}
+}
---
+++ Assets/RankingServer/lib/Scripts/RankingSystem.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Specialized;
+using UnityEngine;
+using System.Net;
+using System.IO;
+using System;
+using UnityEngine.Events;
+
+namespace RankingServer {
+	[Serializable]
+	public class RankingSystem {
+
+		/* POST URL */
+		private readonly string URL = "http://localhost:3000/rankings";
+		private Uri post_uri;
+
+		private string _access_game_key;
+		private string _access_token;
+
+
+		public event Action OnPost;
+		private WebClient wb;
+
+		public RankingSystem (string key, string token) {
+			_access_game_key = key;
+			_access_token    = token;
+			Init ();
+		}
+
+		private void Init() {
+			/* Web Client Initialize */
+			post_uri = new Uri( URL + "?key=" + _access_game_key + "&access_token=" + _access_token );
+			wb = new WebClient ();
+			wb.Headers.Add (HttpRequestHeader.ContentType, "application/json");
+			wb.UploadStringCompleted += Result;
+		}
+
+		public void Send (string nickname, string score) {
+			string data = " { \"ranking\" : { \"nickname\" : \"" + nickname + "\", \"score\" : \""+ score + "\" } }";
+			wb.UploadStringAsync (post_uri, "POST", data);
+		}
+
+		private string ToJson(string nickname, string score) {
+			return " { \"ranking\" : { \"nickname\" : \"" + nickname + "\", \"score\" : \""+ score + "\" } }";
+		}
+
+		private void Result (object s, UploadStringCompletedEventArgs e) {
+			if (e.Error == null) {
+				var result = e.Result;
+				if (OnPost != null) {
+					OnPost.Invoke ();
+				}
+			} else {
+				// error function
+				Debug.Log("Error! : " + e.Result);
+			}
+		}
+	}
+}
